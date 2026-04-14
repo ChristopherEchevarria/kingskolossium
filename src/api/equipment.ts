195fc644816cf759a540ea42a4ac81c84583e2c6
ChristopherEchevarria <christopherechevarria99@gmail.com>
2026-04-14 12:09:38 -0700

    Update: support for card sub components and search bug fix.

--- src/api/equipment.ts
+++ src/api/equipment.ts
@@ -52,6 +52,7 @@ export interface CharacteristicName {
   characteristic_id: number;
   keyword:           string;
   name:              string;
+  operator:          string;   // "+" or "-"
 }
 
 // ─── API Methods ─────────────────────────────────────────────────────────────
